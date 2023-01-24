c12771a627fd4e5de05093d09badf6d700b9b279
sumeeth_regonda <rsumeeth@gmail.com>
2023-01-24 22:25:17 +0530

    functionality added

--- src/todo.js
+++ src/todo.js
@@ -1,37 +1,49 @@
-import React from 'react'
-import { Button, TextField, List, ListItem } from '@mui/material';
+import React from "react";
+import { Button, TextField, List, ListItem } from "@mui/material";
+import { useState } from "react";
 
 const Todo = () => {
+  const [listItems, setListItems] = React.useState([]);
+  const [search, setsearch] = React.useState();
+  const handleChangeText = (event) => {
+    const searchValue = event.target.value;
+    setsearch(searchValue);
+  };
 
-    const [listItems, setListItems] = React.useState([]);
+  const handleAdd = () => {
+    const newListItems = [...listItems, search];
+    setListItems(newListItems);
+  };
 
-    const handleChangeText = (event) => {
-        console.log(event.target.value);
-        //TODO: Add search value to useState variable.
-    }
+  const handleDelete = (index) => {
+    delete listItems[index];
+    setListItems([...listItems]);
+  };
+  return (
+    <div>
+      <div>
+        <TextField
+          onChange={handleChangeText}
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+        <Button onClick={handleAdd}>Add</Button>
+      </div>
+      <List>
+        {listItems?.map(
+          (item, index) =>
+            item && (
+              <ListItem key={index}>
+                {item}{" "}
+                <Button onClick={() => handleDelete(index)}>Delete</Button>
+              </ListItem>
+            )
+        )}
+        {listItems.length === 0 && <ListItem> No Items </ListItem>}
+      </List>
+    </div>
+  );
+};
 
-    const handleAdd = () => {
-        //TODO: Add items to your list
-    }
-
-    return (
-        <div>
-            <div>
-                <TextField onChange={handleChangeText} id="outlined-basic" label="Outlined" variant="outlined" />
-                <Button onClick={handleAdd}>Add</Button>
-            </div>
-            <List>
-                {
-                    listItems?.map(item=> <ListItem> {item} </ListItem>) 
-                }
-                {
-                    listItems.length===0 && <ListItem> No Items </ListItem>
-                }
-            </List>
-
-
-        </div>
-    )
-}
-
-export default Todo
+export default Todo;
