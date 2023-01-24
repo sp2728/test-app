0ec14b11b3b9f3f29255c0325fe308415c6e29cd
sp2728 <sp2728@njit.edu>
2023-01-24 10:14:02 -0500

    branch commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React App
+          Learn React App1231
         </a>
       </header>
     </div>
